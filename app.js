6ea4302f8d3e7d229fa3d5205c0d1ba56b25f6df
Ryan Russell <russerya@gmail.com>
2016-11-07 15:15:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ process.env.TZ = 'America/Los_Angeles';
 
 //API Information
 var weather_host = "http://api.wunderground.com/api/"; //Weather API host
-var apiKey = process.env.WUAPI //Wunderground API Key (Environment Variable)
+var apiKey = process.env.WUAPIKEY //Wunderground API Key (Environment Variable)
 
 /*  Function: weatherAPI
  *  Inputs: path: (Wunderground API path) (See WUnderground API Documentation)
